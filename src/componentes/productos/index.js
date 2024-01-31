aaba7c547e7f9b461dd193961a9e69e203d79e59
Daniel Parada <99292894+DanielParada95@users.noreply.github.com>
2024-01-31 20:16:59 -0300

    Add files via upload

--- src/componentes/productos/index.js
+++ src/componentes/productos/index.js
@@ -1,9 +1,68 @@
 import React from "react";
+import Img from "../images/Img1.PNG"
 
 export const Productoslist = () =>{
     return (
-        <div>
-            <h1>Productos</h1>
+        <>
+        <h1 className="title">Productos</h1>
+        <div className="producto">
+            <div className="productos">
+                <a href="#">
+                <div className="productos-img">
+                    <img src={Img} alt=""/>            
+                </div>
+                </a>
+                <div className="producto__footer">
+                    <h1 > Title </h1>
+                    <p> Categoria </p>
+                    <p className="price">$ tu cola</p>
+                </div>
+                <div className="buttom">
+                    <button className="btn">Anadir al carrito</button>
+                    <div>
+                        <a href="#" className="btn">Ver</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="productos">
+                <a href="#">
+                <div className="productos-img">
+                    <img src={Img} alt=""/>            
+                </div>
+                </a>
+                <div className="producto__footer">
+                    <h1 > Title </h1>
+                    <p> Categoria </p>
+                    <p className="price">$ tu cola</p>
+                </div>
+                <div className="buttom">
+                    <button className="btn">Anadir al carrito</button>
+                    <div>
+                        <a href="#" className="btn">Ver</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="productos">
+                <a href="#">
+                <div className="productos-img">
+                    <img src={Img} alt=""/>            
+                </div>
+                </a>
+                <div className="producto__footer">
+                    <h1 > Title </h1>
+                    <p> Categoria </p>
+                    <p className="price">$ tu cola</p>
+                </div>
+                <div className="buttom">
+                    <button className="btn">Anadir al carrito</button>
+                    <div>
+                        <a href="#" className="btn">Ver</a>
+                    </div>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
